88bc81aa9489d49605fe6ae7b0c6fabad09b5cfd
kuwan90 <149163064+kuwan90@users.noreply.github.com>
2023-11-15 15:47:07 +0900

    課題用ディレクトリ「kadai_web_app」をアップロード

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHPの基礎を学ぼう_27～29章課題</title>
+</head>
+<body>
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+
+    <button onclick="location.href='form.php';">戻る</button>
+</body>
+</html>
